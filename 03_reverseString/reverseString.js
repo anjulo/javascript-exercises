b45de385bfc2ce9c2816ee8a6fab31434bafdcc4
bereket <iberekety@gmail.com>
2022-10-06 13:44:00 -0400

    Implement task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,36 @@
-const reverseString = function() {
+const reverseString = function(str) {
+
+    /*
+    if ( str.length == 0){
+        return "";
+    }
+    else {
+        let strArray = str.split("");
+        strArray.reverse();
+        let reversedString = strArray.join("");
+        return reversedString;
+    }
+    */
+
+    // The above algorithm decoded in one line
+    //return str.split("").reverse().join("")
+
+    // recursion: start from front
+    /*
+    if((str.length == 0) || (str.length == 1)){
+        return(str);
+    }
+    return(str.charAt(str.length-1) + reverseString(str.slice(0,-1)));
+    */
+
+    // recursion: start from back
+    if((str.length == 0) || (str.length == 1)){
+        return(str);
+    }
+    return( reverseString(str.slice(1))+ str.charAt(0) );
+    
+
 
 };
-
 // Do not edit below this line
 module.exports = reverseString;
