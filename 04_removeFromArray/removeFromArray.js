2f5b1b2d284e2d10b6be6f67d867b034e37b2411
bereket <iberekety@gmail.com>
2022-10-06 13:44:20 -0400

    Implement task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function( ) {
 
+    array = arguments[0]; // the array
+    for (let i = 1; i<arguments.length; i++) {
+        while((firstInstanceIndex = array.indexOf(arguments[i])) != -1){
+            array.splice(firstInstanceIndex,1);
+            //firstInstanceIndex = array.indexOf(arguments[i]);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
